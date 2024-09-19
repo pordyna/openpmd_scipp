5f938ebc32071e44251a8948dba6758ffd2d1e98
Paweł Ordyna <p.ordyna@hzdr.de>
2024-09-19 16:22:54 +0200

    fix last fix

--- src/openpmd_scipp/utils.py
+++ src/openpmd_scipp/utils.py
@@ -6,18 +6,18 @@ def _unit_dimension_to_scipp(unit_dimension):
     # (length L, mass M, time T, electric current I, thermodynamic temperature theta,
     # amount of substance N, luminous intensity J)
     base_units = (
-        1 * sc.Unit('m'),
-        1 * sc.Unit('kg'),
-        1 * sc.Unit('s'),
-        1 * sc.Unit('A'),
-        1 * sc.Unit('K'),
-        1 * sc.Unit('mol'),
-        1 * sc.Unit('cd'),
+        1.0 * sc.Unit('m'),
+        1.0 * sc.Unit('kg'),
+        1.0 * sc.Unit('s'),
+        1.0 * sc.Unit('A'),
+        1.0 * sc.Unit('K'),
+        1.0 * sc.Unit('mol'),
+        1.0 * sc.Unit('cd'),
     )
     unit = 1.0 * sc.Unit('1')
     for dim, base_unit in zip(unit_dimension, base_units):
         if dim != 0:
-            unit *= float(base_unit)**dim
+            unit *= base_unit**dim
     return unit.unit
 
 def closest(data, dim, val):
